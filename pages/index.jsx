2ea9b7ad6874c84ab8c4b7d86de4460636a86f72
Vincent <Vincent.bouton.dev@gmail.com>
2022-03-20 13:14:36 +0100

    adding theme

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+const HomePage = () => {
+  return(
+    <div className="bg-dark text-neutral">
+      dark
+      <div className="bg-neutral text-dark">
+        neutral
+      </div>
+      <div className="bg-secondary">
+        secondary
+      </div>
+      <div className="bg-primary">
+        primary 
+      </div>
+    </div>
+    
+  )
+}
+export default HomePage
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#e34a27',
+        secondary: '#7D8491',
+        neutral: '#F7F7F9',
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+}
