b19c3ac1a979e4a868f066c1128c2bce730e6892
Vincent <Vincent.bouton.dev@gmail.com>
2022-03-21 15:14:47 +0100

    Responsive

--- components/Illustration.jsx
+++ components/Illustration.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Illustration = (props) => {
   return (
-    <figure style={"size" in props ? { width:props.size } : {width:"200px"}}>
-      <Image src={props.image} alt={`${ props.caption } Illustration`} layout="responsive"/>
+    <figure>
+      <Image src={props.image} alt={`${ props.caption } Illustration`} layout="intrinsic" width="500" height="500"/>
       <figcaption className="sr-only">{props.caption}</figcaption>
     </figure>
   )
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ import WiPDark from "../public/progress/WiP-Dark.gif"
 import WiPWhite from "../public/progress/WiP-white.gif"
 
 const HomePage = () => {
-  let dark = true
+  let dark = false
   return(
     <div className={dark ? "dark" : ""}>
       <div className="bg-neutral text-dark dark:bg-dark dark:text-neutral min-h-screen">
@@ -16,11 +16,11 @@ const HomePage = () => {
           <Header dark={dark}/> 
         </div>
         <main className="container m-auto">
-          <h1 className="text-6xl max-w-max mx-auto p-2">Work in Progress</h1>
-          <div className="mx-auto max-w-max pl-14">
+          <h1 className="text-4xl max-w-max mx-auto px-2 pt-4">Work in Progress</h1>
+          <div className="mx-auto max-w-max transition-all md:pl-14">
             <Illustration size={500} caption="Boy standing at desk working on his computer" image={dark ? WiPDark : WiPWhite}/>
           </div>  
-          <p className="text-xl text-center w-96 mx-auto">The website is under construction, do not hesitate to come back later.</p>
+          <p className="text-xl text-center sm:w-96 mx-auto">The website is under construction, do not hesitate to come back later.</p>
           <div className="mt-4">
             <Link href="/contact">
               <div className="max-w-max py-4 px-4 mx-auto text-xl transition text-primary rounded-sm hover:bg-primary hover:bg-opacity-20 hover:cursor-pointer">
