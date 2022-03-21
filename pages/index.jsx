83cf4bc008323ddbcec674bfbeae5c6ac56c7a0e
Vincent <Vincent.bouton.dev@gmail.com>
2022-03-21 15:30:16 +0100

    SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,18 +16,21 @@ const HomePage = () => {
           <Header dark={dark}/> 
         </div>
         <main className="container m-auto">
-          <h1 className="text-4xl max-w-max mx-auto px-2 pt-4">Work in Progress</h1>
-          <div className="mx-auto max-w-max transition-all md:pl-14">
-            <Illustration size={500} caption="Boy standing at desk working on his computer" image={dark ? WiPDark : WiPWhite}/>
-          </div>  
-          <p className="text-xl text-center sm:w-96 mx-auto">The website is under construction, do not hesitate to come back later.</p>
-          <div className="mt-4">
-            <Link href="/contact">
-              <div className="max-w-max py-4 px-4 mx-auto text-xl transition text-primary rounded-sm hover:bg-primary hover:bg-opacity-20 hover:cursor-pointer">
-                Contact Me
-              </div>
-            </Link>
-          </div>
+          <h1 className="text-3xl max-w-max mx-auto p-3 md:text-6xl">Welcome to my Website</h1>
+          <section>
+            <h2 className="text-2xl md:text-4xl text-center">Work in Progress</h2>
+            <div className="mx-auto max-w-max transition-all md:pl-14">
+              <Illustration size={500} caption="Boy standing at desk working on his computer" image={dark ? WiPDark : WiPWhite}/>
+            </div>  
+            <p className="text-xl text-center sm:w-96 mx-auto">The website is under construction, do not hesitate to come back later.</p>
+            <div className="mt-4">
+              <Link href="/contact">
+                <div className="max-w-max py-4 px-4 mx-auto text-xl transition text-primary rounded-sm hover:bg-primary hover:bg-opacity-20 hover:cursor-pointer" tabIndex="0">
+                  Contact Me
+                </div>
+              </Link>
+            </div>
+          </section>
         </main>
       </div>
     </div>
