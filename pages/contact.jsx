84ebf877c379ce9ad3f98749571c772c5b71f94d
Vincent <Vincent.bouton.dev@gmail.com>
2022-03-21 16:11:30 +0100

    contact page

---
+++ pages/contact.jsx
@@ -0,0 +1,44 @@
+import Header from "../components/Header"
+import Image from "next/image"
+import Mail from "../public/mail.png"
+import Phone from "../public/phone-call.png"
+import In from "../public/linkedin.png"
+
+const contact = () => {
+  return (
+    <div className="bg-dark text-neutral h-screen">
+      <Header dark={true}/>
+      <main className="container mx-auto">
+        <h1 className="text-4xl text-center md:text-6xl border-b-2 border-neutral max-w-max mx-auto p-3">Need to contact me ?</h1>
+        <section>
+          <h2 className="sr-only">Contact</h2>
+          <address className="w-3/4 mx-auto text-lg mt-6 md:w-3/5 lg:w-2/6">
+            <div className="flex pt-6 flex-wrap">
+              <figure className="mr-4 pt-4">
+                <Image src={Mail} alt="Mail Icon" width="32" height="32"/>
+                <figcaption className="sr-only">White mail flat icon</figcaption>
+              </figure>
+              <a href="mailto:vincent.bouton.dev@gmail.com" className="text-primary hover:bg-primary hover:bg-opacity-20 transition rounded-sm py-4 px-4">vincent.bouton.dev@gmail.com</a>
+            </div>
+            <div className="flex py-6 flex-wrap">
+              <figure className="mr-4 pt-4">
+                <Image src={Phone} alt="Phone Icon" width="32" height="32"/>
+                <figcaption className="sr-only">White phone with waves to simulate a call flat icon</figcaption>
+              </figure>
+              <a href="tel:+32471327836" className="text-primary hover:bg-primary hover:bg-opacity-20 transition rounded-sm py-4 px-4">+32 471 32 78 36</a>
+            </div>
+            <div className="flex flex-wrap">
+              <figure className="mr-4 pt-4">
+                <Image src={In} alt="Linkedin Icon" width="32" height="32"/>
+                <figcaption className="sr-only">Rounded White square with letters "I" and "N" inside</figcaption>
+              </figure>
+              <a href="https://www.linkedin.com/in/vincent-btn-dev/" className="text-primary hover:bg-primary hover:bg-opacity-20 transition rounded-sm py-4 px-4">Vincent Bouton</a>
+            </div>
+          </address>
+        </section>
+      </main>
+    </div> 
+  )
+}
+
+export default contact
